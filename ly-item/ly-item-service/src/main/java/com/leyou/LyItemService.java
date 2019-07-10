9c98e1780704da48a395ff249d067da1cf739b41
451352878 <451352878@qq.com>
2019-07-10 14:30:18 +0800

    第二次提交 测试没问题

--- ly-item/ly-item-service/src/main/java/com/leyou/LyItemService.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/LyItemService.java
@@ -1,15 +1,13 @@
 package com.leyou;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableDiscoveryClient
-@EnableTransactionManagement
 public class LyItemService {
     public static void main(String[] args) {
-        SpringApplication.run(LyItemService.class,args);
+        SpringApplication.run(LyItemService.class, args);
     }
 }
